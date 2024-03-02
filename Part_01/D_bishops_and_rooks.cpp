26b1794c82aeabcc49385862b7271c8431710855
Ruslan <izya.man18@gmail.com>
2024-03-02 23:13:51 +0300

    решил задание Е и небольшие изменения в D

--- Part_01/D_bishops_and_rooks.cpp
+++ Part_01/D_bishops_and_rooks.cpp
@@ -1,53 +1,3 @@
-/*
-На шахматной доске стоят слоны и ладьи, необходимо посчитать, сколько клеток не бьется ни одной из фигур.
-
-Шахматная доска имеет размеры 8 на 8. Ладья бьет все клетки горизонтали и вертикали, проходящих через клетку, где она стоит, 
-до первой встретившейся фигуры. Слон бьет все клетки обеих диагоналей, проходящих через клетку, где он стоит, до первой встретившейся фигуры.
-
-Формат ввода
-В первых восьми строках ввода описывается шахматная доска. 
-Первые восемь символов каждой из этих строк описывают состояние соответствующей горизонтали: символ B (заглавная латинская буква) означает, 
-что в клетке стоит слон, символ R — ладья, символ * — что клетка пуста. 
-После описания горизонтали в строке могут идти пробелы, однако длина каждой строки не превышает 250 символов. После описания доски в файле могут быть пустые строки.
-
-Формат вывода
-Выведите количество пустых клеток, которые не бьются ни одной из фигур.
-
-Пример 1
-Ввод	                Вывод
-********                49
-********
-*R******
-********
-********
-********
-********
-********
-
-Пример 2
-Ввод	                Вывод
-********                54
-********
-******B*
-********
-********
-********
-********
-********
-
-Пример 3
-Ввод	                Вывод
-********                40
-*R******
-********
-*****B**
-********
-********
-********
-********
-
-*/
-
 #include <iostream>
 #include <vector>
 
@@ -57,27 +7,33 @@ int CntB(vector<string>& table, int x, int y) {
     int res_cnt = 0;
     if (x > 0 && y > 0) {
         for (int i = x - 1, j = y - 1; i >= 0, j >= 0; --i, --j) {
-            if (table[j][i] == '*') res_cnt++;
+            if (j < 0 || i < 0) break;
+            if (table[j][i] == '*') { res_cnt++; table[j][i] = '-'; }
+            else if (table[j][i] == '-') continue;
             else break;
         } 
     }
     if (x < 7 && y < 7) {
         for (int i = x + 1, j = y + 1; i < 8, j < 8; ++i, ++j) {
-            if (table[j][i] == '*') res_cnt++;
+            if (j > 7 || i > 7) break;
+            if (table[j][i] == '*') { res_cnt++; table[j][i] = '-'; }
+            else if (table[j][i] == '-') continue;
             else break;
         } 
     }
     if (y > 0 && x < 7) {
         for (int j = y - 1, i = x + 1; j >= 0, i < 8; --j, ++i) {
             if (j < 0 || i > 7) break;
-            if (table[j][i] == '*') res_cnt++;
+            if (table[j][i] == '*') { res_cnt++; table[j][i] = '-'; }
+            else if (table[j][i] == '-') continue;
             else break;
         }
     }
     if (y < 7 && x > 0) {
         for (int i = x - 1, j = y + 1; i >= 0, j < 8; ++j, --i) {
             if (j > 7 || i < 0) break;
-            if (table[j][i] == '*') res_cnt++;
+            if (table[j][i] == '*') { res_cnt++; table[j][i] = '-'; }
+            else if (table[j][i] == '-') continue;
             else break;
         } 
     }
@@ -88,25 +44,29 @@ int CntR(vector<string>& table, int x, int y) {
     int res_cnt = 0;
     if (x > 0) {
         for (int i = x - 1; i >= 0; --i) {
-            if (table[y][i] == '*') res_cnt++;
+            if (table[y][i] == '*') { res_cnt++; table[y][i] = '-'; }
+            else if (table[y][i] == '-') continue;
             else break;
         } 
     }
     if (x < 7) {
         for (int i = x + 1; i < 8; ++i) {
-            if (table[y][i] == '*') res_cnt++;
+            if (table[y][i] == '*') { res_cnt++; table[y][i] = '-'; }
+            else if (table[y][i] == '-') continue;
             else break;
         } 
     }
     if (y > 0) {
         for (int i = y - 1; i >= 0; --i) {
-            if (table[i][x] == '*') res_cnt++;
+            if (table[i][x] == '*') { res_cnt++; table[i][x] = '-'; }
+            else if (table[i][x] == '-') continue;
             else break;
         }
     }
     if (y < 7) {
         for (int i = y + 1; i < 8; ++i) {
-            if (table[i][x] == '*') res_cnt++;
+            if (table[i][x] == '*') { res_cnt++; table[i][x] = '-'; }
+            else if (table[i][x] == '-') continue;
             else break;
         } 
     }
@@ -121,14 +81,14 @@ int main()
         cin >> buf;
         table.push_back(buf);
     }
-    int res_cnt = 0;
+    int res_cnt = 0, cnt_pers = 0;
     
     for (int y = 0; y < 8; ++y) {
         for (int x = 0; x < 8; ++x) {
-            if (table[y][x] == 'B') res_cnt += CntB(table, x, y);
-            if (table[y][x] == 'R') res_cnt += CntR(table, x, y);
+            if (table[y][x] == 'B') { res_cnt += CntB(table, x, y); cnt_pers++; }
+            if (table[y][x] == 'R') { res_cnt += CntR(table, x, y); cnt_pers++; }
         }
     }
-    cout << res_cnt << '\n';
+    cout << 64 - cnt_pers - res_cnt << '\n';
     return 0;
 }
